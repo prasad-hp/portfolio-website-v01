dd902990da4648c98870a0912d6f2e6ad7e194d8
Prasad Hp <prasadhp@outlook.in>
2024-04-24 21:08:35 +0530

    Moved all the output files to public

--- public/index.js
+++ public/index.js
@@ -3,15 +3,13 @@
 function showMenu(){
     const menuOption = document.getElementById("menuToggle")
     const menuIcon = document.getElementById("menuIcon")
-    // menuOption.classList.remove("hidden")
-    // menuOption.classList.toggle("inline")
     if(menuOption.className === "hidden"){
         menuOption.className = "inline"
-        menuIcon.setAttribute("src", "../images/clear_white_24dp.svg ")
+        menuIcon.setAttribute("src", "./images/clear_white_24dp.svg ")
 
     }else{
         menuOption.className = "hidden"
-        menuIcon.setAttribute("src", "../images/menu_white_24dp.svg ")
+        menuIcon.setAttribute("src", "./images/menu_white_24dp.svg ")
     }
 
 }
