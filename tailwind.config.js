cfffff3a0bfbb7d2940a61ab6149fd84a5f9e70c
Prasad Hp <prasadhp@outlook.in>
2024-04-24 14:12:44 +0530

    Rewriting the Website with Basic HTML, Tailwind CSS and JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      "navy" : "#0a192f",
+      "Light-Navy": "#112240",
+      "Lightest-Navy":	"#233554",
+      "Slate":"#8892b0",
+      "Light-Slate":"#a8b2d1",
+      "Lightest-Slate": "#ccd6f6",
+      "Light-White":"#e6f1ff",
+      "Green":"#64ffda",
+    },
   },
   plugins: [],
 }
-
